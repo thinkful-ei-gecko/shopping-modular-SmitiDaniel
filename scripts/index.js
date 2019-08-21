d6b2dad317f7d40e14914cd027c2872bfa329795
SmitiShakya <smitishk12@gmail.com>
2019-08-21 10:20:37 -0500

    Added store module

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,20 @@
 'use strict';
-/* global shoppingList, cuid */
+/* global shoppingList*/
 
 // eslint-disable-next-line no-unused-vars
-const store = {
-  items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
+
+
 
 function main() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 }
 
+
+console.log(store);
+
+
+
 $(main);
+
+
